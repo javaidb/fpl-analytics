c4ef24fd92559fdcd181a24a9af7ea75c4541932
javaidb <javaidbaksh@gmail.com>
2024-03-01 13:09:29 -0500

    Completely removed previous approach of total_summary (finally) and replaced with master_summary. Rewrote league_data structuring.

--- src/functions/raw_data_compiler.py
+++ src/functions/raw_data_compiler.py
@@ -2,7 +2,7 @@
 import pandas as pd
 from tqdm.notebook import tqdm_notebook
 from src.functions.helper_fns import calculate_mean_std_dev
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 import asyncio
 import aiohttp
@@ -14,21 +14,22 @@ class RawDataCompiler:
         self.api_parser = api_parser
         self.raw_data = api_parser.raw_data
         self.master_summary = self.build_master_summary()
-        self.total_summary = None
+        # self.total_summary = None
         self.players = pd.json_normalize(self.raw_data['elements'])
         self.teams = pd.json_normalize(self.raw_data['teams'])
         self.positions = pd.json_normalize(self.raw_data['element_types'])
         self.team_info = self.get_team_info()
-        self.total_summary = asyncio.run(self.compile_dataframes())
-        self.effective_ownership = self.initialize_effective_ownership()
+        # self.total_summary = asyncio.run(self.compile_dataframes())
+        self.league_data = self.initialize_league_data()
     
     def get_team_info(self):
-        teamdata = self.raw_data['teams']
-        list_of_dicts = [x for x in teamdata]
+        list_of_dicts = [x for x in self.teams]
         return pd.DataFrame.from_records(list_of_dicts)
 
-#======================================= BUILD MASTER SUMMARY ====================================================
-    
+#================================================================================================================================================================
+#===================================================================== BUILD MASTER SUMMARY =====================================================================
+#================================================================================================================================================================
+
     def grab_full_history(self):
         raw_data = self.api_parser.full_element_summary
         return [gw_data for player_id in sorted(self.api_parser.player_ids) for gw_data in raw_data[player_id]['history']]
@@ -78,169 +79,56 @@ class RawDataCompiler:
                 consolidated_dict[player_id][f'pos_{raw_data_col}'] = raw_data_val
 
         return {player_id: dict(data) for player_id, data in consolidated_dict.items()}
-
-######################################################################################################################
-    # async def fetch(session, url):
-    #     async with session.get(url) as response:
-    #         return await response.json()
-
-    async def fetch_gameweek_history(self, player_id, session):
-        if player_id in self.api_parser.raw_element_summary:
-            player_history = self.api_parser.raw_element_summary[player_id]['history']
-            return player_history
-
-        player_data = await self.api_parser.fetch_element_summaries(player_id, session)
-        player_history = player_data['history']
-        return player_history
-
-    async def compile_dataframes(self):
-        self.players = self.players.merge(
-            self.teams[['id', 'name']],
-            left_on='team',
-            right_on='id',
-            suffixes=['_player', None]
-        ).drop(['team', 'id'], axis=1)
-
-        self.players = self.players.merge(
-            self.positions[['id', 'singular_name_short']],
-            left_on='element_type',
-            right_on='id'
-        ).drop(['element_type', 'id'], axis=1)
-        player_ids = self.players['id_player'].tolist()
-
-        async def fetch_all_gameweek_histories(player_ids):
-            async with aiohttp.ClientSession() as session:
-                tasks = [self.fetch_gameweek_history(player_id, session) for player_id in player_ids]
-                return await asyncio.gather(*tasks)
-
-        points = await fetch_all_gameweek_histories(player_ids)
-        total_summary = self.compile_total_df(points)
-        return total_summary
-
-    def compile_total_df(self, points):
-        self.total_points_df = pd.concat([pd.DataFrame(x) for x in points])
-        self.points = self.players[['id_player', 'web_name','singular_name_short']].merge(
-            self.total_points_df,
-            left_on='id_player',
-            right_on='element'
-        )
-
-        self.partial_summary = self.points.groupby(
-            ['id_player', 'web_name']
-        ).agg(
-            {'total_points':'sum', 'goals_scored':'sum', 'assists':'sum','bonus':'sum'}
-        ).reset_index(
-        ).sort_values(
-            'total_points', ascending=False
-        )
-        self.points.rename(columns = {'singular_name_short':'position','web_name':'player'}, inplace = True)
-        self.partial_summary.rename(columns = {'singular_name_short':'position','web_name':'player'}, inplace = True)
-        overlapping_columns = self.partial_summary.columns.intersection(self.players.columns).tolist()    
-        total_summary = pd.merge(self.partial_summary,self.players,on=overlapping_columns)
-
-        async def compile_summary():
-            async with aiohttp.ClientSession() as session:
-                tasks = [self.fetch_gameweek_history(x['id'],session) for x in self.api_parser.raw_data['elements']]
-                gameweek_histories = await asyncio.gather(*tasks)
-
-            def process_binary_returns(dfx):
-                return [2 if (i > 9) else 1 if (i > 3 and i <= 9) else 0 for i in dfx['total_points']]
-
-            def process_fulltime_fullhour(dfx):
-                fulltime = [e for e in dfx['minutes'][-6:] if e == 90]
-                fullhour = [e for e in dfx['minutes'][-6:] if e >= 60]
-                return len(fulltime), len(fullhour), len(dfx['minutes'][-6:])
-            
-            def calc_rolling_avg(lookback: int, dfx: pd.DataFrame):
-                return sum(dfx[-lookback:]) / len(dfx[-lookback:])
-
-            self.master_summary_temp = []
-            for num, idx in enumerate(tqdm_notebook(total_summary['id_player'], desc="Building master dataframe")):
-                try:
-                    player_data_history = gameweek_histories[num]
-                    for player_gw_data in player_data_history:
-                        self.master_summary_temp.append(player_gw_data)
-                    
-                    player_data_df = pd.DataFrame(data=player_data_history).copy()
-                    existing_cols = player_data_df.columns.tolist()
-
-                    for param in existing_cols[1:]:
-                        p1, p2 = param, param
-                        if param == 'total_points':
-                            p1 = 'history'
-                        elif param == 'value':
-                            p1 = 'changing_value'
-                        ref_list = player_data_df[p2].tolist()
-                        converted_ref_list = []
-                        for elem in ref_list:
-                            try:
-                                converted_ref_list.append(float(elem))
-                            except:
-                                converted_ref_list.append(elem)
-                        total_summary.at[[num],p1] = pd.Series([converted_ref_list],index = [num])
-                    returnhist = process_binary_returns(player_data_df)
-
-                    mean_og, stdev_og = calculate_mean_std_dev(player_data_df['total_points'])
-                    mean_x, stdev_x = calculate_mean_std_dev(returnhist)
-                    last6_mean_x, last3_mean_x, last2_mean_x = calc_rolling_avg(6,returnhist), calc_rolling_avg(3,returnhist), calc_rolling_avg(2,returnhist)
-
-                    fulltime_count, fullhour_count, total_minutes = process_fulltime_fullhour(player_data_df)
-                    total_summary.at[num,'value'] = self.points.loc[self.points['id_player'] == idx]['value'].iloc[-1] / 10
-                    total_summary.at[num, 'fulltime'] = f"{fulltime_count}/{total_minutes}"
-                    total_summary.at[num, 'fullhour'] = f"{fullhour_count}/{total_minutes}"
-                    total_summary.at[[num], 'returnhist'] = pd.Series([returnhist],index = [num])
-                    total_summary.at[num, 'mean_og'] = mean_og
-                    total_summary.at[num, 'stdev_og'] = stdev_og
-                    total_summary.at[num, 'mean_x'] = mean_x
-                    total_summary.at[num, 'stdev_x'] = stdev_x
-                    total_summary.loc[num,'last6_mean_x'] = last6_mean_x
-                    total_summary.loc[num,'last3_mean_x'] = last3_mean_x
-                    total_summary.loc[num,'last2_mean_x'] = last2_mean_x
-
-                except Exception as e:
-                    print(f'{num} : {e}')
-                    continue
-        asyncio.run(compile_summary())
-        total_summary.rename(columns = {'singular_name_short':'position'}, inplace = True)
-        if not total_summary.empty:
-            total_summary.to_csv('../../stats/total_summary.csv', index=False)
-            print('Exported total_summary!')
-        return total_summary
-######################################################################################################################    
     
-    def initialize_effective_ownership(self):    
-        rival_league_ids = [x for x in self.api_parser.get_league_ids()]
-        rank_thresholds = [{"name": "beacon_1k", "rank": 1000}, {"name": "beacon_10k", "rank": 10000}, {"name": "beacon_100k", "rank": 100000}]
+    def initialize_league_data(self):
+        
+        pseudo_league_data = [
+            {"name": "beacon_aggregate", "symbol": "☆", "id": None, "custom_info": None},
+            {"name": "beacon_1k", "symbol": "☆₁ₖ", "id": None, "custom_info": {"rank": 1000}},
+            {"name": "beacon_10k", "symbol": "☆₁₀ₖ", "id": None, "custom_info": {"rank": 10000}},
+            {"name": "beacon_100k", "symbol": "☆₁₀₀ₖ", "id": None, "custom_info": {"rank": 100000}}
+        ]
+        specified_league_data = [
+            {
+                "name": str(league_data["id"]),
+                "symbol": league_data["symbol"],
+                "id": league_data["id"],
+                "custom_info": None,
+                }
+            for league_data in self.api_parser.config_data["fpl_id_data"]["personal_league_ids"]
+        ]
+                
+        personal_user_name = self.api_parser.fetch_player_fpl_name(self.api_parser.get_personal_fpl_id())
+        points_window = 50
 
-        def rank_finder(ID):
-            r = self.api_parser.fetch_data_from_api(f'entry/{ID}/event/{self.api_parser.latest_gw}/picks/')
-            return r['entry_history']['overall_rank']
         grouped_user_ids_by_league={}
-        for rank_thresh in rank_thresholds:
-            ids_that_cross_rank = [idx for idx in self.api_parser.get_beacon_ids() if rank_finder(idx) <= rank_thresh["rank"]]
-            grouped_user_ids_by_league[rank_thresh["name"]] = ids_that_cross_rank
-        for rival_league_id in rival_league_ids:
-            league_r = self.api_parser.fetch_data_from_api(f'leagues-classic/{rival_league_id}/standings/')
-            league_players = league_r['standings']['results']
-            my_rank, my_points = [(x['rank'],x['total']) for x in league_players if x['player_name'] == 'Javaid Baksh'][0]
-            user_ids = [x['entry'] for x in league_players if ((x['rank'] < my_rank) or (x['total'] > my_points - 50 and x['rank'] > my_rank))]
-            grouped_user_ids_by_league[str(rival_league_id)] = user_ids
-        grouped_user_ids_by_league['beacon_aggregate'] = [idx for idx in self.api_parser.get_beacon_ids()]
+        for league_data in pseudo_league_data:
+            if league_data["custom_info"] is not None:
+                ids_that_cross_rank = [idx for idx in self.api_parser.get_beacon_ids() if self.api_parser.fetch_player_overall_fpl_rank(idx) <= league_data["custom_info"]["rank"]]
+                grouped_user_ids_by_league[league_data["name"]] = ids_that_cross_rank
+            elif league_data["name"] == "beacon_aggregate":
+                grouped_user_ids_by_league[league_data["name"]] = [idx for idx in self.api_parser.get_beacon_ids()]
 
-        def dict_counter(dictx, listx, rivalx):
-            for idx in listx:
-                if idx not in dictx[rivalx]['players'].keys():
-                    dictx[rivalx]['players'][idx] = 1
-                else:
-                    dictx[rivalx]['players'][idx] += 1
-            return
+        for rival_league_data in specified_league_data:
+            league_r = self.api_parser.fetch_data_from_api(f'leagues-classic/{rival_league_data["id"]}/standings/')
+            league_players = league_r["standings"]["results"]
+            my_rank, my_points = [(x["rank"], x["total"]) for x in league_players if x["player_name"] == personal_user_name][0]
+            user_ids = [x["entry"] for x in league_players if ((x["rank"] < my_rank) or (x["total"] > my_points - points_window and x["rank"] > my_rank))]
+            grouped_user_ids_by_league[rival_league_data["name"]] = user_ids
 
-        counter_dict = {}
-        for pseudo_league_name, user_ids in grouped_user_ids_by_league.items():
-            counter_dict[pseudo_league_name] = {'rivals':len(user_ids),'players':{}}
-            for user_id in user_ids:
-                r = self.api_parser.fetch_data_from_api(f'entry/{user_id}/event/{self.api_parser.latest_gw}/picks/')
-                player_ids = [x['element'] for x in r['picks']]
-                dict_counter(counter_dict, player_ids, pseudo_league_name)
-            counter_dict[pseudo_league_name]['players'] = dict(sorted(counter_dict[pseudo_league_name]['players'].items(), key=lambda item: item[1], reverse=True))
-        return counter_dict
+        agg_league_data = pseudo_league_data + specified_league_data
+
+        def compile_ownership(league_info):
+
+            def fetch_latest_picks(user_id):
+                r = self.api_parser.fetch_fpl_data(user_id)["latest_picks"]
+                return [x["element"] for x in r["picks"]]
+
+            user_ids = grouped_user_ids_by_league[league_info["name"]]
+            league_info["summary"] = {
+                "rivals": user_ids,
+                "players": dict(Counter(player_id for user_id in user_ids for player_id in fetch_latest_picks(user_id)))
+            }
+            return league_info
+
+        return [compile_ownership(league_info.copy()) for league_info in agg_league_data]
