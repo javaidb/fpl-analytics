f1aafc443e41a4fcfd205f9e64e009c9ae7f20b0
javaidb <javaidbaksh@gmail.com>
2024-04-29 21:43:53 -0400

    Added bools to outline whether to create if dir does not exist and outputting to json.

--- src/functions/data_exporter.py
+++ src/functions/data_exporter.py
@@ -1,7 +1,11 @@
 import os
+import json
 
+def output_data_to_json(data, file_path):
+    with open(file_path, 'w') as json_file:
+        json.dump(data, json_file)
 
-def grab_path_relative_to_root(path_from_root: str, relative=False, absolute=False):
+def grab_path_relative_to_root(path_from_root: str, relative=False, absolute=False, create_if_nonexistent=False):
     """
     Find the path provided from the root, whether it is relative to the current location or from the drive (such as C:), and create if not found.
     The root in this case is considered as the name of the git repository that houses other folders.
@@ -15,9 +19,9 @@ def grab_path_relative_to_root(path_from_root: str, relative=False, absolute=Fal
     root_path, rel_path_to_root = _find_paths_to_root()
 
     if relative:
-        return _ensure_directory_exists(os.path.join(rel_path_to_root, path_from_root))
+        return _ensure_directory_exists(os.path.join(rel_path_to_root, path_from_root)) if create_if_nonexistent else os.path.join(rel_path_to_root, path_from_root)
     elif absolute:
-        return _ensure_directory_exists(os.path.join(root_path, path_from_root))
+        return _ensure_directory_exists(os.path.join(root_path, path_from_root)) if create_if_nonexistent else os.path.join(rel_path_to_root, path_from_root)
     else: print("One of 'absolute' or 'relative' needs to be True")
 
 def _find_paths_to_root():
