273e5fd102e6ec51585345832f8963ed43b390e9
javaidb <javaidbaksh@gmail.com>
2024-11-24 00:51:15 -0500

    Cosmetic changes, ordering, and legibility.

--- src/functions/raw_data_fetcher.py
+++ src/functions/raw_data_fetcher.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath('..'))
 from src.config import config
 from src.functions.data_exporter import output_data_to_json, grab_path_relative_to_root
-from src.functions.helper_fns import initialize_local_data
+from src.functions.helper_utils import initialize_local_data
 
 import requests
 import json
@@ -16,7 +16,7 @@ from dateutil import parser
 
 import asyncio
 import aiohttp
-from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
+# from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_type
 from understat import Understat
 from understatapi import UnderstatClient
 
@@ -30,20 +30,21 @@ This allows for async contexts to be used synchronously, e,g, i can now use raw_
 
 class FPLFetcher:
     def __init__(self):
+        print("Fetching data from FPL API.")
         self.base_url = config.BASE_URL
-        self.raw_data, self.season_year_span_id = self.process_raw_data()
-        self.latest_gw = self.get_latest_gameweek()
-        self.player_ids = self.grab_player_ids()
+        self.raw_data, self.season_year_span_id = self._process_raw_data()
+        self.latest_gw = self._get_latest_gameweek()
+        self.player_ids = self._grab_player_ids()
         self.raw_element_summary = {}
-        self.config_data = self.get_config_data()
-        self.rival_ids = self.fetch_rivals_based_on_pts()
-        self.personal_fpl_raw_data = self.fetch_personal_fpl_data()
-        self.fixtures = self.fetch_fixtures()
+        self.config_data = self._get_config_data()
+        self.rival_ids = self._fetch_rivals_based_on_pts()
+        self.personal_fpl_raw_data = self._fetch_personal_fpl_data()
+        self.fixtures = self._fetch_fixtures()
         # self.blanks, self.dgws = self.look_for_blanks_and_dgws()
-        self.rival_stats = self.tabulate_rival_stats(self.get_beacon_ids())
-        self.full_element_summary = asyncio.run(self.compile_master_element_summary())
+        self.rival_stats = self._tabulate_rival_stats(self._get_beacon_ids())
+        self.full_element_summary = asyncio.run(self._compile_master_element_summary())
 
-    def process_raw_data(self):
+    def _process_raw_data(self):
         raw_data = self.fetch_data_from_api('bootstrap-static/')
         season_year_span_id = self.get_season_year_span(raw_data)
         raw_data_path = f"cached_data/fpl/{season_year_span_id}"
@@ -52,6 +53,45 @@ class FPLFetcher:
         full_path = f'{file_path_written}/{raw_data_file_name}.json'
         output_data_to_json(raw_data, full_path)
         return raw_data, season_year_span_id
+    
+    def _get_latest_gameweek(self):
+        gwdata = self.raw_data['events']
+        now = datetime.now(timezone.utc)
+        latest_gw = max(
+            (int(gw['id']) for gw in gwdata if parser.parse(gw['deadline_time']) <= now),
+            default=None
+        )
+        return latest_gw
+    
+    def _grab_player_ids(self):
+        return [x['id'] for x in self.raw_data['elements']]
+    
+    def _get_config_data(self):
+        config_data_path = grab_path_relative_to_root("src/config/data.json", relative=True)
+        with open(config_data_path, 'r') as file:
+            data = json.load(file)
+            return data
+
+    def _fetch_rivals_based_on_pts(self):
+        rival_team_ids = []
+        personal_fpl_name = self.fetch_player_fpl_name(self._get_personal_fpl_id())
+        for league_id in self._get_league_ids():
+            r = self.fetch_data_from_api('leagues-classic/' + str(league_id) + '/standings/')
+            rank_threshold = next((i['rank'] for i in iter(r['standings']['results']) if ['player_name'] == personal_fpl_name), None)
+            if rank_threshold:
+                rival_ids = []
+                for i in r['standings']['results']:
+                    if i['rank'] < min(rank_threshold,5):
+                        rival_ids.append(i['entry'])
+                rival_team_ids.append(rival_ids)
+        return rival_team_ids
+
+    def _fetch_personal_fpl_data(self):
+        fpl_id = self._get_personal_fpl_id()
+        return self.fetch_fpl_data(fpl_id)
+    
+    def _fetch_fixtures(self):
+        return self.fetch_data_from_api('fixtures/')
 
     def fetch_data_from_api(self, endpoint):
         url = f'{self.base_url}{endpoint}'
@@ -63,15 +103,6 @@ class FPLFetcher:
         #         data = await response.json()
         #         return data
     
-    def get_latest_gameweek(self):
-        gwdata = self.raw_data['events']
-        now = datetime.now(timezone.utc)
-        latest_gw = max(
-            (int(gw['id']) for gw in gwdata if parser.parse(gw['deadline_time']) <= now),
-            default=None
-        )
-        return latest_gw
-    
     def get_season_year_span(self, raw_data):
         datetime_strings = [x['deadline_time'] for x in raw_data['events']]
         years = [datetime.strptime(dt_str, '%Y-%m-%dT%H:%M:%SZ').year for dt_str in datetime_strings]
@@ -81,41 +112,18 @@ class FPLFetcher:
             return str(start_year)
         else:
             return f"{start_year}-{end_year}"
-    
-    def grab_player_ids(self):
-        return [x['id'] for x in self.raw_data['elements']]
-    
-    def get_config_data(self):
-        config_data_path = grab_path_relative_to_root("src/config/data.json", relative=True)
-        with open(config_data_path, 'r') as file:
-            data = json.load(file)
-            return data
 
-    def get_league_ids(self):
+    def _get_league_ids(self):
         active_szn = self.config_data["fpl_id_data"]
         return [x["id"] for x in active_szn["personal_league_ids"]]
 
-    def get_beacon_ids(self):
+    def _get_beacon_ids(self):
         active_szn = self.config_data["fpl_id_data"]
         return active_szn["beacon_team_ids"]
 
-    def get_personal_fpl_id(self):
+    def _get_personal_fpl_id(self):
         active_szn = self.config_data["fpl_id_data"]
         return active_szn["personal_team_id"]
-
-    def fetch_rivals_based_on_pts(self):
-        rival_team_ids = []
-        personal_fpl_name = self.fetch_player_fpl_name(self.get_personal_fpl_id())
-        for league_id in self.get_league_ids():
-            r = self.fetch_data_from_api('leagues-classic/' + str(league_id) + '/standings/')
-            rank_threshold = next((i['rank'] for i in iter(r['standings']['results']) if ['player_name'] == personal_fpl_name), None)
-            if rank_threshold:
-                rival_ids = []
-                for i in r['standings']['results']:
-                    if i['rank'] < min(rank_threshold,5):
-                        rival_ids.append(i['entry'])
-                rival_team_ids.append(rival_ids)
-        return rival_team_ids
 
     def fetch_fpl_data(self, fpl_id):
         fpl_info = self.fetch_data_from_api(f"entry/{fpl_id}")
@@ -128,21 +136,54 @@ class FPLFetcher:
         }
 
     def fetch_player_overall_fpl_rank(self, ID):
+        """
+        Function used to fetch a users rank across all of FPL users via their ID.
+
+        Parameters:
+        - ID (int): FPL ID
+
+        Returns:
+        - overall FPL rank
+        """
         r = self.fetch_fpl_data(ID)
         return r["general_info"]["summary_overall_rank"]
 
     def fetch_player_fpl_name(self, ID):
+        """
+        Function used to fetch a users name via their ID.
+
+        Parameters:
+        - ID (int): FPL ID
+
+        Returns:
+        - FPL Account Name
+        """
         r = self.fetch_fpl_data(ID)["general_info"]
         first_name = r["player_first_name"]
         last_name = r["player_last_name"]
         return f"{first_name} {last_name}"
-
-    def fetch_personal_fpl_data(self):
-        fpl_id = self.get_personal_fpl_id()
-        return self.fetch_fpl_data(fpl_id)
     
-    def fetch_fixtures(self):
-        return self.fetch_data_from_api('fixtures/')
+    async def _compile_master_element_summary(self):
+        full_element_summary = {}
+        async def fetch_all_summaries():
+            async with aiohttp.ClientSession() as session:
+                tasks = [self.fetch_gameweek_player_data(player_id,session) for player_id in sorted(self.player_ids)]
+                gameweek_histories = await asyncio.gather(*tasks)
+            for player_num, player_id in enumerate(tqdm_notebook(sorted(self.player_ids), desc="Building element summaries")):
+                try:
+                    player_data = gameweek_histories[player_num]
+                    full_element_summary[player_id] = player_data
+                except Exception as e:
+                    print(f"Error with element summary building for ID {player_id}: {e}")
+        asyncio.run(fetch_all_summaries())
+        return full_element_summary
+    
+    async def fetch_gameweek_player_data(self, player_id, session):
+        if player_id in self.raw_element_summary:
+            return self.raw_element_summary[player_id]
+
+        player_data = await self.fetch_element_summaries(player_id, session)
+        return player_data
     
     async def fetch_element_summaries(self, player_id: int, session = None):
         if player_id in self.raw_element_summary:
@@ -169,45 +210,9 @@ class FPLFetcher:
         self.raw_element_summary[player_id] = player_data
         return player_data
     
-    async def fetch_gameweek_player_data(self, player_id, session):
-        if player_id in self.raw_element_summary:
-            return self.raw_element_summary[player_id]
-
-        player_data = await self.fetch_element_summaries(player_id, session)
-        return player_data
-    
-    async def compile_master_element_summary(self):
-        full_element_summary = {}
-        async def fetch_all_summaries():
-            async with aiohttp.ClientSession() as session:
-                tasks = [self.fetch_gameweek_player_data(player_id,session) for player_id in sorted(self.player_ids)]
-                gameweek_histories = await asyncio.gather(*tasks)
-            for player_num, player_id in enumerate(tqdm_notebook(sorted(self.player_ids), desc="Building element summaries")):
-                try:
-                    player_data = gameweek_histories[player_num]
-                    full_element_summary[player_id] = player_data
-                except Exception as e:
-                    print(f"Error with element summary building for ID {player_id}: {e}")
-        asyncio.run(fetch_all_summaries())
-        return full_element_summary
-    
 ##########################################################################################################################
-
-    def compile_rivals_team(self, team_ids: list):
-        rival_ids_data = {}
-        for team_id in team_ids:
-            r = self.fetch_data_from_api(f'entry/{team_id}/')
-            first_name = r['player_first_name']
-            last_name = r['player_last_name']
-            rival_ids_data[team_id] = {'name': f'{first_name} {last_name}',
-                                  'points': r['summary_overall_points'],
-                                  'rank': r['summary_overall_rank']}
-        for rival_id in rival_ids_data.keys():
-            r = self.fetch_data_from_api(f'entry/{rival_id}/event/{self.latest_gw}/picks/')
-            rival_ids_data[rival_id]['team'] = [i['element'] for i in r['picks']]
-        return rival_ids_data
     
-    def tabulate_rival_stats(self, team_id_list: list):
+    def _tabulate_rival_stats(self, team_id_list: list):
         rival_id_data = self.compile_rivals_team(team_id_list)
         self.rival_id_data = rival_id_data
         list_rival_FPLteam_ids = []
@@ -225,60 +230,73 @@ class FPLFetcher:
                     id_count[ide] += 1
         return {'unique_ids': unique_ids, 'similar_ids': similar_ids, 'id_count': id_count}
 
-class UnderstatFetcher:
+    def compile_rivals_team(self, team_ids: list):
+        rival_ids_data = {}
+        for team_id in team_ids:
+            r = self.fetch_data_from_api(f'entry/{team_id}/')
+            first_name = r['player_first_name']
+            last_name = r['player_last_name']
+            rival_ids_data[team_id] = {'name': f'{first_name} {last_name}',
+                                  'points': r['summary_overall_points'],
+                                  'rank': r['summary_overall_rank']}
+        for rival_id in rival_ids_data.keys():
+            r = self.fetch_data_from_api(f'entry/{rival_id}/event/{self.latest_gw}/picks/')
+            rival_ids_data[rival_id]['team'] = [i['element'] for i in r['picks']]
+        return rival_ids_data
 
-    def __init__(self, fpl_api_parser, fpl_helper_fns, update_and_export_data):
-        self.fpl_api_parser = fpl_api_parser
-        self.helper_fns = fpl_helper_fns
+class UnderstatFetcher(FPLFetcher):
+
+    def __init__(self, update_and_export_data):
+        super().__init__()
         self.understat_ops = initialize_local_data(self, [
             {
                 "function": self._build_understat_team_data,
                 "attribute_name": "understat_team_data",
                 "file_name": "team_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/team",
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/team",
              },
             {
                 "function": self._match_fpl_to_understat_teams,
                 "attribute_name": "understat_to_fpl_team_data",
                 "file_name": "understat_to_fpl_team_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/team",
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/team",
              },
             {
                 "function": self._build_understat_team_shot_data,
                 "attribute_name": "understat_team_shot_data",
                 "file_name": "team_shot_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/team"
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/team"
              },
             {
                 "function": self._build_understat_team_match_data,
                 "attribute_name": "understat_team_match_data",
                 "file_name": "team_match_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/team"
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/team"
              },
             {
                 "function": self._build_understat_player_data,
                 "attribute_name": "understat_player_data",
                 "file_name": "player_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/players"
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/players"
              },
             {
                 "function": self._match_fpl_to_understat_players,
                 "attribute_name": "understat_to_fpl_player_data",
                 "file_name": "understat_to_fpl_player_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/players",
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/players",
                 "update_bool_override": True,
              },
             {
                 "function": self._build_understat_player_shot_data,
                 "attribute_name": "understat_player_shot_data_raw",
                 "file_name": "player_shot_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/players"
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/players"
              },
             {
                 "function": self._build_understat_player_match_data,
                 "attribute_name": "understat_player_match_data",
                 "file_name": "player_match_data",
-                "export_path": f"cached_data/understat/{self.fpl_api_parser.season_year_span_id}/players"
+                "export_path": f"cached_data/understat/{self.season_year_span_id}/players"
              }
         ], update_and_export_data)
 
@@ -371,7 +389,7 @@ class UnderstatFetcher:
 #================================================================================================================================================================
 
     def _match_fpl_to_understat_teams(self):
-        teams = self.fpl_api_parser.raw_data['teams']
+        teams = self.raw_data['teams']
         fpl_data = [{**{param_str: param_val for param_str, param_val in team.items() if param_str in ['id', 'name']}} for team in teams]
 
         understat_nums_unsorted = [{
@@ -483,7 +501,7 @@ class UnderstatFetcher:
         for fpl_player_id in all_matched_fpl_player_ids:
             player_shot_data = self.understat_player_shot_data_raw[fpl_player_id]
 
-            season_threshold_start_str = next(x['deadline_time'] for x in iter(self.fpl_api_parser.raw_data['events']))
+            season_threshold_start_str = next(x['deadline_time'] for x in iter(self.raw_data['events']))
             def convert_dtstr_to_dt(input_str: str):
                 adjusted_str = datetime.fromisoformat(input_str.replace('Z', ''))
                 return datetime.strptime(adjusted_str.strftime('%Y-%m-%d'), '%Y-%m-%d')
