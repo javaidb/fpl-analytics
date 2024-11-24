286381030f4c336133ecc3345fcad9ad18bd9865
javaidb <javaidbaksh@gmail.com>
2024-11-24 00:19:22 -0500

    Structure changed, now inherits methods, etc., from FPL API fetcher, as well as reads utils from new module. Other minor changes.

--- src/functions/data_builder.py
+++ src/functions/data_builder.py
@@ -1,36 +1,38 @@
-from src.functions.helper_fns import calculate_mean_std_dev, progress_bar_update
-from src.functions.helper_fns import initialize_local_data
+# from src.functions.helper_fns import calculate_mean_std_dev, progress_bar_update
+from src.functions.helper_utils import initialize_local_data
+
+from src.functions.raw_data_fetcher import FPLFetcher
 
 from collections import defaultdict, Counter
-from tqdm.notebook import tqdm_notebook
+# from tqdm.notebook import tqdm_notebook
 import pandas as pd
-import time
 
 import nest_asyncio
 nest_asyncio.apply()
 
-class RawDataCompiler:
-    def __init__(self, api_parser):
-        self.api_parser = api_parser
-        self.master_summary = self.build_master_summary()
+class FPLRawDataCompiler(FPLFetcher):
+    def __init__(self):
+        super().__init__()
+        print("Building master datasets from raw data via FPL API.")
+        self.master_summary = self._build_master_summary()
         initialize_local_data(self, [
             {
                 "function": self.convert_fpl_dict_to_tabular,
                 "attribute_name": "master_summary_tabular",
-                "file_name": f"master_summary_{self.api_parser.season_year_span_id}",
-                "export_path": f"cached_data/fpl/{self.api_parser.season_year_span_id}",
+                "file_name": f"master_summary_{self.season_year_span_id}",
+                "export_path": f"cached_data/fpl/{self.season_year_span_id}",
             }
         ], update_and_export_data = True)
         # self.master_summary = self.build_master_summary()
         # self.total_summary = None
-        self.players = pd.json_normalize(self.api_parser.raw_data['elements'])
-        self.teams = pd.json_normalize(self.api_parser.raw_data['teams'])
-        self.positions = pd.json_normalize(self.api_parser.raw_data['element_types'])
-        self.team_info = self.get_team_info()
+        self.players = pd.json_normalize(self.raw_data['elements'])
+        self.teams = pd.json_normalize(self.raw_data['teams'])
+        self.positions = pd.json_normalize(self.raw_data['element_types'])
+        self.team_info = self._get_team_info()
         # self.total_summary = asyncio.run(self.compile_dataframes())
-        self.league_data = self.initialize_league_data()
+        self.league_data = self._initialize_league_data()
     
-    def get_team_info(self):
+    def _get_team_info(self):
         list_of_dicts = [x for x in self.teams]
         return pd.DataFrame.from_records(list_of_dicts)
 
@@ -38,13 +40,9 @@ class RawDataCompiler:
 #===================================================================== BUILD MASTER SUMMARY =====================================================================
 #================================================================================================================================================================
 
-    def grab_full_history(self):
-        raw_data = self.api_parser.full_element_summary
-        return [gw_data for player_id in sorted(self.api_parser.player_ids) for gw_data in raw_data[player_id]['history']]
-
-    def build_master_summary(self):
-        print("Building master summary ...")
-        id_values = sorted(self.api_parser.player_ids)
+    def _build_master_summary(self):
+        print("Building master summary.")
+        id_values = sorted(self.player_ids)
         elem_summaries = self.grab_full_history()
         rel_elem_summaries = [x for x in elem_summaries if x['element'] in id_values]
         rel_elem_summaries = sorted(rel_elem_summaries, key=lambda x: x['round'])
@@ -64,14 +62,14 @@ class RawDataCompiler:
             consolidated_dict[player_id]['round'].append(round_num)
             
         #Add additional info from bootstrap raw data
-        bootstrap_dict = self.api_parser.raw_data['elements']
+        bootstrap_dict = self.raw_data['elements']
         for player_id in consolidated_dict.keys():
             for raw_data_col in ['team', 'element_type', 'first_name', 'second_name', 'web_name', 'id']:
                 raw_data_val = next((x[raw_data_col] for x in iter(bootstrap_dict) if x['id'] == player_id))
                 consolidated_dict[player_id][raw_data_col] = raw_data_val
         
         #Add additional info from teams
-        bootstrap_teams = self.api_parser.raw_data['teams']
+        bootstrap_teams = self.raw_data['teams']
         for player_id, player_data in consolidated_dict.items():
             team_id = player_data["team"]
             for raw_data_col in ['short_name', 'name', 'strength_overall_home', 'strength_overall_away', 'strength_attack_home', 'strength_attack_away', 'strength_defence_home', 'strength_defence_away']:
@@ -79,7 +77,7 @@ class RawDataCompiler:
                 consolidated_dict[player_id][f'team_{raw_data_col}'] = raw_data_val
         
         #Add additional info from positions
-        bootstrap_pos = self.api_parser.raw_data['element_types']
+        bootstrap_pos = self.raw_data['element_types']
         for player_id, player_data in consolidated_dict.items():
             pos_id = player_data["element_type"]
             for raw_data_col in ['singular_name_short']:
@@ -87,7 +85,11 @@ class RawDataCompiler:
                 consolidated_dict[player_id][f'pos_{raw_data_col}'] = raw_data_val
 
         return {player_id: dict(data) for player_id, data in consolidated_dict.items()}
-    
+
+    def grab_full_history(self):
+        raw_data = self.full_element_summary
+        return [gw_data for player_id in sorted(self.player_ids) for gw_data in raw_data[player_id]['history']]
+
     def convert_fpl_dict_to_tabular(self):
         df_data = []
         for _, player_data in self.master_summary.items():
@@ -110,7 +112,7 @@ class RawDataCompiler:
                     data_per_gw[append_col] = append_data
         return df_data
 
-    def initialize_league_data(self):
+    def _initialize_league_data(self):
         
         pseudo_league_data = [
             {"name": "beacon_aggregate", "symbol": "☆", "id": None, "custom_info": None},
@@ -125,22 +127,22 @@ class RawDataCompiler:
                 "id": league_data["id"],
                 "custom_info": None,
                 }
-            for league_data in self.api_parser.config_data["fpl_id_data"]["personal_league_ids"]
+            for league_data in self.config_data["fpl_id_data"]["personal_league_ids"]
         ]
                 
-        personal_user_name = self.api_parser.fetch_player_fpl_name(self.api_parser.get_personal_fpl_id())
+        personal_user_name = self.fetch_player_fpl_name(self._get_personal_fpl_id())
         points_window = 50
 
         grouped_user_ids_by_league={}
         for league_data in pseudo_league_data:
             if league_data["custom_info"] is not None:
-                ids_that_cross_rank = [idx for idx in self.api_parser.get_beacon_ids() if self.api_parser.fetch_player_overall_fpl_rank(idx) <= league_data["custom_info"]["rank"]]
+                ids_that_cross_rank = [idx for idx in self._get_beacon_ids() if self.fetch_player_overall_fpl_rank(idx) <= league_data["custom_info"]["rank"]]
                 grouped_user_ids_by_league[league_data["name"]] = ids_that_cross_rank
             elif league_data["name"] == "beacon_aggregate":
-                grouped_user_ids_by_league[league_data["name"]] = [idx for idx in self.api_parser.get_beacon_ids()]
+                grouped_user_ids_by_league[league_data["name"]] = [idx for idx in self._get_beacon_ids()]
 
         for rival_league_data in specified_league_data:
-            league_r = self.api_parser.fetch_data_from_api(f'leagues-classic/{rival_league_data["id"]}/standings/')
+            league_r = self.fetch_data_from_api(f'leagues-classic/{rival_league_data["id"]}/standings/')
             league_players = league_r["standings"]["results"]
             my_rank, my_points = [(x["rank"], x["total"]) for x in league_players if x["player_name"] == personal_user_name][0]
             user_ids = [x["entry"] for x in league_players if ((x["rank"] < my_rank) or (x["total"] > my_points - points_window and x["rank"] > my_rank))]
@@ -151,7 +153,7 @@ class RawDataCompiler:
         def compile_ownership(league_info):
 
             def fetch_latest_picks(user_id):
-                r = self.api_parser.fetch_fpl_data(user_id)["latest_picks"]
+                r = self.fetch_fpl_data(user_id)["latest_picks"]
                 return [x["element"] for x in r["picks"]]
 
             user_ids = grouped_user_ids_by_league[league_info["name"]]
