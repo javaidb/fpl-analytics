e12fe691c6ab6931004e88937f8efdb42522213f
javaidb <javaidbaksh@gmail.com>
2024-11-28 18:56:47 -0500

    Added understat compiler for data_builder (used in dev notebooks for now) + renamed some vars to reflect best representation of data.

--- src/functions/data_builder.py
+++ src/functions/data_builder.py
@@ -1,9 +1,15 @@
 # from src.functions.helper_fns import calculate_mean_std_dev, progress_bar_update
 from src.functions.helper_utils import initialize_local_data
 
-from src.functions.raw_data_fetcher import FPLFetcher
+from src.functions.raw_data_fetcher import FPLFetcher, UnderstatFetcher
+# from src.functions.notebook_utils import setup_logger, log_timing
 
+# logger = setup_logger(__name__)
+
+from tqdm.notebook import tqdm_notebook
+from understatapi import UnderstatClient
 from collections import defaultdict, Counter
+import datetime
 # from tqdm.notebook import tqdm_notebook
 import pandas as pd
 
@@ -15,6 +21,7 @@ class FPLRawDataCompiler(FPLFetcher):
         super().__init__()
         print("Building master datasets from raw data via FPL API.")
         self.master_summary = self._build_master_summary()
+        # logger.info(f"Testing logger: {self.master_summary}")
         initialize_local_data(self, [
             {
                 "function": self.convert_fpl_dict_to_tabular,
@@ -25,21 +32,21 @@ class FPLRawDataCompiler(FPLFetcher):
         ], update_and_export_data = True)
         # self.master_summary = self.build_master_summary()
         # self.total_summary = None
-        self.players = pd.json_normalize(self.raw_data['elements'])
-        self.teams = pd.json_normalize(self.raw_data['teams'])
-        self.positions = pd.json_normalize(self.raw_data['element_types'])
-        self.team_info = self._get_team_info()
+        self.players_raw = pd.json_normalize(self.raw_data['elements'])
+        self.teams_raw = pd.json_normalize(self.raw_data['teams'])
+        self.positions_raw = pd.json_normalize(self.raw_data['element_types'])
+        self.team_info_raw = self._get_team_info()
         # self.total_summary = asyncio.run(self.compile_dataframes())
         self.league_data = self._initialize_league_data()
     
     def _get_team_info(self):
-        list_of_dicts = [x for x in self.teams]
+        list_of_dicts = [x for x in self.teams_raw]
         return pd.DataFrame.from_records(list_of_dicts)
 
 #================================================================================================================================================================
 #===================================================================== BUILD MASTER SUMMARY =====================================================================
 #================================================================================================================================================================
-
+    # @log_timing
     def _build_master_summary(self):
         print("Building master summary.")
         id_values = sorted(self.player_ids)
@@ -164,3 +171,136 @@ class FPLRawDataCompiler(FPLFetcher):
             return league_info
 
         return [compile_ownership(league_info.copy()) for league_info in agg_league_data]
+    
+class UnderstatRawDataCompiler(UnderstatFetcher):
+    def __init__(self, fpl_helper_fns, update_and_export_data):
+        self.find_best_match_fpl = fpl_helper_fns.find_best_match
+        self.grab_player_name_fpl = fpl_helper_fns.grab_player_name_fpl
+        self.raw_data_fpl = fpl_helper_fns.raw_data
+        super().__init__(fpl_helper_fns, update_and_export_data)
+        print("Mapping FPL to understat data.")
+        initialize_local_data(self, [
+            {
+                "function": self._match_fpl_to_understat_teams,
+                "attribute_name": "understat_to_fpl_team_data",
+                "file_name": "understat_to_fpl_team_data",
+                "export_path": f"cached_data/understat/{fpl_helper_fns.season_year_span_id}/team",
+             },
+            {
+                "function": self._match_fpl_to_understat_players,
+                "attribute_name": "understat_to_fpl_player_data",
+                "file_name": "understat_to_fpl_player_data",
+                "export_path": f"cached_data/understat/{fpl_helper_fns.season_year_span_id}/players",
+                "update_bool_override": True,
+             },
+            {
+                "function": self._build_understat_player_shot_data,
+                "attribute_name": "understat_player_shot_data",
+                "file_name": "player_shot_data",
+                "export_path": f"cached_data/understat/{fpl_helper_fns.season_year_span_id}/players"
+             },
+            {
+                "function": self._build_understat_player_match_data,
+                "attribute_name": "understat_player_match_data",
+                "file_name": "player_match_data",
+                "export_path": f"cached_data/understat/{fpl_helper_fns.season_year_span_id}/players"
+             }
+        ], update_and_export_data)
+
+#================================================================================================================================================================
+#===================================================================== MATCH FPL TO UNDERSTAT ===================================================================
+#================================================================================================================================================================
+
+    def _match_fpl_to_understat_teams(self):
+        teams = self.raw_data_fpl['teams']
+        fpl_data = [{**{param_str: param_val for param_str, param_val in team.items() if param_str in ['id', 'name']}} for team in teams]
+
+        understat_nums_unsorted = [{
+            "id": int(x['id']), 
+            "name": x['title']
+        } 
+        for x in self.understat_team_data_raw.values()]
+        understat_data = sorted(understat_nums_unsorted, key=lambda x: x["name"])
+
+        return [{
+            "fpl": d1, 
+            "understat": d2
+        } 
+        for d1, d2 in zip(fpl_data, understat_data)]
+    
+    def _match_fpl_to_understat_players(self):
+
+        matched_data = []
+        for understat_player_info in self.understat_player_data_raw:
+            team_name_list = understat_player_info["team_title"].split(",") #Account for cases where player switched teams in PL
+            # print(f"{team_name_list}")
+            fpl_team_id_list = [next((x["fpl"]["id"] for x in iter(self.understat_to_fpl_team_data) if x["understat"]["name"] == team_name), 'null') for team_name in team_name_list]
+            matched_fpl_player_id = self.find_best_match_fpl(understat_player_info["player_name"], fpl_team_id_list)
+            if matched_fpl_player_id is not None:
+                matched_data.append({
+                    "fpl": {
+                        "id": int(matched_fpl_player_id),
+                        "name": self.grab_player_name_fpl(int(matched_fpl_player_id)),
+                    },
+                    "understat": {
+                        "id": int(understat_player_info["id"]),
+                        "name": understat_player_info["player_name"],
+                    }
+                })
+        return matched_data
+    
+        
+    def _build_understat_player_shot_data(self):
+
+        # async def main():
+        #     async with aiohttp.ClientSession() as session:
+        #         understat = Understat(session)
+        #         player = await understat.get_player_shots(
+        #             player_id=8562, 
+        #         )
+        #         return player
+        
+        # loop = asyncio.get_event_loop()
+        # fetched_data = loop.run_until_complete(main())
+
+        all_matched_fpl_player_ids = [x['fpl']['id'] for x in self.understat_to_fpl_player_data]
+        all_player_shot_data = {}
+        for fpl_player_id in tqdm_notebook(all_matched_fpl_player_ids, desc = "Building understat player shot data"):
+            understat_id = next(x["understat"]["id"] for x in iter(self.understat_to_fpl_player_data) if x["fpl"]["id"] == int(fpl_player_id))
+            all_player_shot_data[int(fpl_player_id)] = self.understat_player_shot_data_raw.get(f"{understat_id}")
+        return all_player_shot_data
+
+    def _compile_understat_player_shot_data(self):
+
+        all_matched_fpl_player_ids = [x['fpl']['id'] for x in self.understat_to_fpl_player_data]
+        all_player_shot_data = {}
+        for fpl_player_id in all_matched_fpl_player_ids:
+            player_shot_data = self.understat_player_shot_data_raw[fpl_player_id]
+
+            season_threshold_start_str = next(x['deadline_time'] for x in iter(self.raw_data_fpl['events']))
+            def convert_dtstr_to_dt(input_str: str):
+                adjusted_str = datetime.fromisoformat(input_str.replace('Z', ''))
+                return datetime.strptime(adjusted_str.strftime('%Y-%m-%d'), '%Y-%m-%d')
+            relevant_player_shot_data = [x for x in player_shot_data if convert_dtstr_to_dt(x['date']) >= convert_dtstr_to_dt(season_threshold_start_str)]
+
+            df = pd.DataFrame(relevant_player_shot_data)
+            grouped = df.groupby(['result', 'match_id','h_team','a_team']).size().reset_index(name='count')
+            chance_summary = grouped.pivot_table(index=['match_id','h_team','a_team'], columns='result', values='count', fill_value=0)
+            chance_summary.reset_index(inplace=True)
+            
+            hits_df = df[df["result"] == "Goal"]
+            grouped = hits_df.groupby(['shotType', 'match_id','h_team','a_team']).size().reset_index(name='count')
+            goal_summary = grouped.pivot_table(index=['match_id','h_team','a_team'], columns='shotType', values='count', fill_value=0)
+            goal_summary.reset_index(inplace=True)
+
+            all_player_shot_data[int(fpl_player_id)] = pd.merge(chance_summary, goal_summary, on=['match_id','h_team','a_team'], how='outer').to_dict(orient='records')
+        return all_player_shot_data
+    
+    def _build_understat_player_match_data(self):
+
+        all_matched_fpl_player_ids = [x['fpl']['id'] for x in self.understat_to_fpl_player_data]
+        all_player_match_data = {}
+        for fpl_player_id in tqdm_notebook(all_matched_fpl_player_ids, desc = "Building understat player match data"):
+            understat_id = next(x["understat"]["id"] for x in iter(self.understat_to_fpl_player_data) if x["fpl"]["id"] == int(fpl_player_id))
+            all_player_match_data[int(fpl_player_id)] = self.understat_player_match_data_raw.get(f"{understat_id}")
+        return all_player_match_data
